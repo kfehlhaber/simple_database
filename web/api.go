f2fa435e19700623026d01e43fa56e29309796fa
Kaj Fehlhaber <kaj.fehlhaber@gmail.com>
2019-05-13 20:44:31 +0200

    Fixed violations and removed one travis-run

--- web/api.go
+++ web/api.go
@@ -1,16 +1,19 @@
 package web
 
 import (
-	"fmt"
 	"github.com/kejne/simple_database/db"
+	"log"
 	"net/http"
 )
 
 func Serve() {
 	http.HandleFunc("/get", get)
 	http.HandleFunc("/set", set)
-	fmt.Println("Web API server ready to accept requests")
-	http.ListenAndServe(":80", nil)
+	log.Println("Web API server ready to accept requests")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
 
 func get(w http.ResponseWriter, r *http.Request) {
@@ -25,7 +28,11 @@ func get(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusNotFound)
 		} else {
 			w.WriteHeader(http.StatusOK)
-			w.Write([]byte(response))
+			n, err := w.Write([]byte(response))
+
+			if n < len(response) {
+				log.Fatalln(err.Error())
+			}
 		}
 	}
 }
@@ -35,7 +42,10 @@ func set(w http.ResponseWriter, r *http.Request) {
 	for key := range query {
 		response := []byte(db.Persist(key, query.Get(key)))
 		w.WriteHeader(http.StatusOK)
-		w.Write(response)
+		n, err := w.Write(response)
+		if n < len(response) {
+			log.Fatalln(err.Error())
+		}
 		// Ignore all but first parameter
 		break;
 	}
